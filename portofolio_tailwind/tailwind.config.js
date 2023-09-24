29635ba6688b5f751d7402cd07e5eaf351486ff6
Raka Atmaja <rakapa23@gmail.com>
2023-09-25 01:43:47 +0700

    push lagi

---
+++ portofolio_tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'body': ['Poppins'],
+      'display': ['Helvetica']
+    }
+  },
+  plugins: [],
+}
+
