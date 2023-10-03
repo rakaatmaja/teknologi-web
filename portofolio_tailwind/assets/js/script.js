4b3aaba4bb331e28e01a88b9a357e5b6273f7422
Raka Atmaja <rakapa23@gmail.com>
2023-10-03 16:11:11 +0700

    top

--- portofolio_tailwind/assets/js/script.js
+++ portofolio_tailwind/assets/js/script.js
@@ -35,3 +35,10 @@ function type() {
 }
 type();
 
+
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up')
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll')
+  else scrollUp.classList.remove('show-scroll')
+};
+window.addEventListener('scroll', scrollUp)
